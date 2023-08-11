23b390fd80c46634fa0d044c7d96ceaa2bf47e03
Priyansh Rai <priyanshrai16@gmail.com>
2023-08-11 11:39:08 +0530

    Added icons to cards

--- Level-1/abstract-help-page/src/components/FeatureCard.jsx
+++ Level-1/abstract-help-page/src/components/FeatureCard.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-const FeatureCard = ({ title, description, url }) => {
+const FeatureCard = ({ title, description, url, iconUrl }) => {
     return (
         <section className="feature-card">
-            <h3 className="feature-heading">{title}</h3>
-            <p className="feature-description">{description}</p>
-            <a href={url} className="feature-learn-more">
-                Learn more...
-            </a>
+            <div className="card-image-container">
+                <img src={iconUrl} alt="Card Icon" />
+            </div>
+            <div className="card-text-container">
+                <h3 className="feature-heading">{title}</h3>
+                <p className="feature-description">{description}</p>
+                <a href={url} className="feature-learn-more">
+                    Learn more...
+                </a>
+            </div>
         </section>
     );
 };
--- Level-1/abstract-help-page/src/components/Features.jsx
+++ Level-1/abstract-help-page/src/components/Features.jsx
@@ -9,6 +9,8 @@ const Features = () => {
             description:
                 "Abstract lets you manage, version, and document your designs in one place.",
             url: "/",
+            iconUrl:
+                "https://theme.zdassets.com/theme_assets/2197739/aed6500f4da41c641f3de5d96247eb6654e9c386.png",
         },
         {
             key: 2,
@@ -16,6 +18,8 @@ const Features = () => {
             description:
                 "Configure your account settings, such as your email, profile details, and password.",
             url: "/",
+            iconUrl:
+                "https://theme.zdassets.com/theme_assets/2197739/91588b3d042df901d9b941ccbb57af4f151401c7.png",
         },
         {
             key: 3,
@@ -23,12 +27,16 @@ const Features = () => {
             description:
                 "Use Abstract organizations, teams, and projects to organize your people and your work.",
             url: "/",
+            iconUrl:
+                "https://theme.zdassets.com/theme_assets/2197739/50d0db451c0e1e66e1caddd142986fcf6792ca05.png",
         },
         {
             key: 4,
             title: "Manage billing",
             description: "Change subscriptions and payment details.",
             url: "/",
+            iconUrl:
+                "https://theme.zdassets.com/theme_assets/2197739/3e82d0126e97d7b7c60a611d4e11b3c278d33743.png",
         },
         {
             key: 5,
@@ -36,12 +44,16 @@ const Features = () => {
             description:
                 "Set up and configure SSO, SCIM, and Just-in-Time provisioning.",
             url: "/",
+            iconUrl:
+                "https://theme.zdassets.com/theme_assets/2197739/7712592465ef5c7bf18f1c7cb16e19ab44eb81e3.png",
         },
         {
             key: 6,
             title: "Abstract support",
             description: "Get in touch with a human.",
             url: "/",
+            iconUrl:
+                "https://theme.zdassets.com/theme_assets/2197739/dcc25d18e224d12621d74e545cc8bdd1b5685ca8.png",
         },
     ];
     return (
@@ -52,6 +64,7 @@ const Features = () => {
                         title={feature.title}
                         description={feature.description}
                         url={feature.url}
+                        iconUrl={feature.iconUrl}
                         key={feature.key}
                     />
                 ))}
